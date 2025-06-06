bc621f019c25fa656d84c215e48c22d60ca0d63c
杨天成 <3323892632@qq.com>
2025-06-06 11:49:45 +0800

    修改AuthController，使其实现注册功能与注册后登录的重定向

--- src/main/java/com/example/jpastumanagersys/controller/AuthController.java
+++ src/main/java/com/example/jpastumanagersys/controller/AuthController.java
@@ -1,12 +1,19 @@
 package com.example.jpastumanagersys.controller;
 
+import com.example.jpastumanagersys.entity.User;
+import com.example.jpastumanagersys.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/login")
     public String login() {
         return "login";
@@ -15,5 +22,11 @@ public class AuthController {
     @GetMapping("/register")
     public String register() {
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String processRegister(User user) {
+        userService.saveUser(user);
+        return "redirect:/auth/login";
     }
 }
